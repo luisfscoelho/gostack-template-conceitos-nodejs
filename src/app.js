c502e62ff2c3945e2fc707496f271412be1260b5
Luis Coelho <luis.fs.coelho@pm.me>
2020-04-28 13:30:40 -0300

    feito testes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,59 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { url, title, techs } = request.body;
+
+  const repo = { id: uuid(), url, title, techs, likes: 0 };
+
+  repositories.push(repo);
+
+  return response.json(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { url, title, techs } = request.body;
+
+  const index = repositories.findIndex(repo => repo.id === id);
+
+  if(index === -1)
+    return response.status(400).send('Repository not found');
+
+  const repo = {...repositories[index], url, title, techs};
+
+  repositories[index] = repo;
+
+  return response.json(repo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = repositories.findIndex(repo => repo.id === id);
+
+  if(index === -1)
+    return response.status(400).send('Repository not found');
+
+  repositories.splice(index, 1);
+
+  return response.status(204).send('Deleted')
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = repositories.findIndex(repo => repo.id === id);
+
+  if(index === -1)
+    return response.status(400).send('Repository not found');
+
+  repositories[index].likes++;
+
+  return response.json(repositories[index]);
 });
 
 module.exports = app;
